0483075539379bc34b2bb66a06f3ebcad03e678a
Aditya Rangarajan <rrajanaditya@gmail.com>
2021-06-23 07:19:41 +0530

    First Release

---
+++ README.md
@@ -0,0 +1,10 @@
+A netflix-clone made using React.
+
+To run this app on your PC, you have to install Node and run the following commands in your terminal in the project folder:-
+
+### npm i ###
+
+### npm start ###
+
+This app has been deployed with firebase.
+You can check this out here :- https://netflix-clone-c57b3.web.app
---
+++ src/components/Row.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from '../axios';
+import './Row.css';
+const baseUrl = 'https://image.tmdb.org/t/p/original/';
+
+const Row = (props) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(props.fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [props.fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{props.title}</h2>
+
+            <div className="row__posters">
+                {movies.map((movie) => {
+                    return (
+                        <img
+                            key={movie.id}
+                            className={`row__poster ${props.isLargeRow && "row__posterLarge"}`}
+                            src={`${baseUrl}${props.isLargeRow ? movie.backdrop_path: movie.backdrop_path}`}
+                            alt={movie.name}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Row;
